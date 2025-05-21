eedd0bc1ca76399cbbf1431228a7e6a5d6c3780f
Rimfo4 <108602552+Rimfo4@users.noreply.github.com>
2025-05-21 18:30:50 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int pan = 0;
+        
+        if(7 >= n)  pan = 1;
+        else if(n%7==0)  pan = n/7;
+        else pan = (n/7)+1;
+        
+        return pan;
+    }
+}
