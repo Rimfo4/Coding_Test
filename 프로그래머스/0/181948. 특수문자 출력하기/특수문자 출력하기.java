5080deae82e214da9c19f307e8c03aabd53a65b4
Rimfo4 <108602552+Rimfo4@users.noreply.github.com>
2025-06-04 17:00:02 +0900

    [level 0] Title: 특수문자 출력하기, Time: 255.27 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = "!@#$%^&*(";
+        char b = 92;  // ' \ '
+        char c = 39;  // ' ' '
+        char d = 34;  // ' " '
+        String e ="<>?:;";
+        System.out.print(a+b+c+d+e);
+    }
+}
