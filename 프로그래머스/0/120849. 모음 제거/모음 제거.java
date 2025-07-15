727ef6874c33a871b361371740524f1e2044ba15
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-15 18:19:28 +0900

    [level 0] Title: 모음 제거, Time: 10.71 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if(my_string.charAt(i) == 'a') continue;
+            else if(my_string.charAt(i) == 'e') continue;
+            else if(my_string.charAt(i) == 'i') continue;
+            else if(my_string.charAt(i) == 'o') continue;
+            else if(my_string.charAt(i) == 'u') continue;
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
