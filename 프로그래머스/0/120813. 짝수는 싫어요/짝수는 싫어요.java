d70fa385805ac45572044413d134ecf81e7bed11
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-15 10:10:31 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] solution(int n) {
-        
-        int gijun = n%2==0 ? n/2 : (n/2)+1;
-        int[] answer = new int[gijun];
+        int[] answer = new int[n%2==0 ? n/2 : (n/2)+1];
         int cnt = 0;
         
         for(int i = 1; i <= n; i+=2){
