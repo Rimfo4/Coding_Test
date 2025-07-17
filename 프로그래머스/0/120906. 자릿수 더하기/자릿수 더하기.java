7ac17c4b745e860db2c9b2d019b9177fea0b3778
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-17 21:31:32 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -4,34 +4,33 @@ class Solution {
         int cnt = 0;
         
         int i = n;
-        if(n == 0)
-            answer = 0;
+        if(n == 0) answer = 0;
         else{
-        while(true){                // 자릿수 확인용 반복문
-            if(i >= 10){
-                cnt++;
-                i = i / 10;
-            } else break;
-        }
-        cnt += 1;                   // 자릿수
+            while(true){                // 자릿수 확인용 반복문
+                if(i >= 10){
+                    cnt++;
+                    i = i / 10;
+                } else break;
+            }
+            cnt += 1;                   // 자릿수
         
-        while (true){
-            i = cnt;
-            int a = 1;
-            while(i != 1){
-                a *= 10;
-                i--;
+            while (true){
+                i = cnt;
+                int a = 1;
+                while(i != 1){
+                    a *= 10;
+                    i--;
+                }
+                if(n > a){
+                    answer += n / a;
+                    n = n % a;
+                }
+                if(n < 10 && n > 0){
+                    answer += n;
+                    break;
+                }
+                cnt--;
             }
-            if(n > a){
-                 answer += n / a;
-                 n = n % a;
-            }
-            if(n < 10 && n > 0){
-                answer += n;
-                break;
-            }
-            cnt--;
-        }
         }
         // if(n > 1000){
         //     answer += n / 1000;
