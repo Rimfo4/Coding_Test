4c8a014425f8e57bbb7c63eaeef8bae023630dd6
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-17 20:37:08 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -2,16 +2,12 @@ class Solution {
     public String[] solution(String[] names) {
         int gijun = names.length%5==0 ? names.length/5 : names.length/5+1;
         String[] answer = new String[gijun];
-        
         int cnt = 0;
-        
         for(int i = 0; i < names.length; i++){
-            
             if(i==0 || i%5==0){
                 answer[cnt] = names[i];
                 cnt++;
-            }else
-                continue;
+            }else continue;
         }
         return answer;
     }
