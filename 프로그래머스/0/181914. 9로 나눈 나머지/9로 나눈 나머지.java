ec8bdf6bdf3b01b236162805c7e5f9f27019c040
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-18 15:04:07 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 19.53 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        
+        for(int i = 0; i < number.length(); i++){
+            if(number.charAt(i) == '1') answer += 1;
+            else if(number.charAt(i) == '2') answer += 2;
+            else if(number.charAt(i) == '3') answer += 3;
+            else if(number.charAt(i) == '4') answer += 4;
+            else if(number.charAt(i) == '5') answer += 5;
+            else if(number.charAt(i) == '6') answer += 6;
+            else if(number.charAt(i) == '7') answer += 7;
+            else if(number.charAt(i) == '8') answer += 8;
+            else if(number.charAt(i) == '9') answer += 9;
+        }
+        return answer % 9;
+    }
+}
