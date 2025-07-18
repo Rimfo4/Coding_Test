bb2c40e319feca801d09ee6bb52928fb1b3bf2ab
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-18 14:27:40 +0900

    [level 0] Title: 개미 군단, Time: 0.06 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,19 +1,13 @@
 class Solution {
     public int solution(int hp) {
         int answer = 0;
-        final int king = 5;
-        final int att = 3;
-        final int work = 1;
-        
         if(hp >= 5){
-            answer += hp / king;
-            hp %= king;
+            answer += hp / 5;
+            hp %= 5;
+        }if(hp >= 3){
+            answer += hp / 3;
+            hp %= 3;
         }
-        if(hp >= 3){
-            answer += hp / att;
-            hp %= att;
-        }
-        answer += hp;
-    return answer;
+    return answer += hp;
     }
 }
