dcd4ede8c3326edf5ca9336361aac817a9ce0607
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-19 11:58:36 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,19 +1,17 @@
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
-        int big = s1.length > s2.length ? s1.length : s2.length;
-        int small = s1.length <= s2.length ? s1.length : s2.length;
         
         if(s1.length < s2.length){
-            for(int i = 0; i < small; i++){
-                for(int j = big-1; 0 <= j; j--){
+            for(int i = 0; i < s1.length; i++){
+                for(int j = s2.length-1; 0 <= j; j--){
                     if(s1[i].equals(s2[j])) answer++;
                 }
             }
         }
         else
-            for(int i = 0; i < small; i++){
-                for(int j = big-1; 0 <= j; j--){
+            for(int i = 0; i < s2.length; i++){
+                for(int j = s1.length-1; 0 <= j; j--){
                     if(s2[i].equals(s1[j])) answer++;
                 }
             }
