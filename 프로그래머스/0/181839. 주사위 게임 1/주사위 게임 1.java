194fb3f94872a9ce362722515f527a96438c1a1e
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-27 11:32:10 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -2,14 +2,10 @@ class Solution {
     public int solution(int a, int b) {
         int answer = 0;
         int sum = 0;
-        if(a%2==1 && b%2==1)
-            sum = (a*a) + (b*b);
-        else if(a%2==1 || b%2==1)
-            sum = 2*(a + b);
-        else {
-            if(a < b) sum = b - a;
+        if(a%2==1 && b%2==1) sum = (a*a) + (b*b);
+        else if(a%2==1 || b%2==1) sum = 2*(a + b);
+        else if(a < b) sum = b - a;
             else if(a > b) sum = a - b;
-        }
         return sum;
     }
 }
