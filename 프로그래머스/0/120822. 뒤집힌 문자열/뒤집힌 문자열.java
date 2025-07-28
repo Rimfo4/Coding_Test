d10ca4d748eca22a8a7ee2f9f386582a87414225
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-28 11:35:55 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 12.48 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -2,12 +2,8 @@ class Solution {
     public String solution(String my_string) {
         char[] arr = new char[my_string.length()];
         String a = "";
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = my_string.charAt(i);   
-        }
-        for(int i = arr.length-1; i >= 0; i--){
-            a += arr[i];
-        }
+        for(int i = 0; i < arr.length; i++)arr[i] = my_string.charAt(i);   
+        for(int i = arr.length-1; i >= 0; i--) a += arr[i];
         return a;
     }
 }
