88a0274084508af1067d232157e4efa828a5d4ae
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-29 14:31:03 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         int cnt = 0;
-        
         int i = n;
         if(n == 0) answer = 0;
         else{
@@ -32,22 +31,6 @@ class Solution {
                 cnt--;
             }
         }
-        // if(n > 1000){
-        //     answer += n / 1000;
-        //     n = n % 1000;
-        // }
-        // if(n > 100){
-        //     answer += n / 100;
-        //     n = n % 100;
-        // }
-        // if(n > 10){
-        //     answer += n / 10;
-        //     n = n % 10;
-        // }
-        // if(n > 1){
-        //     answer += n / 1;
-        //     n = n % 1;
-        // }
         return answer;
     }
 }
