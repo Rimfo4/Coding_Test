c58e5f9ece7b6168e0e6908e78c2255d36be3a54
JM <108602552+Rimfo4@users.noreply.github.com>
2025-07-29 14:32:36 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.14 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -2,26 +2,20 @@ class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
         int[] meosse = new int[array.length+1];
-        
-        for(int i =0; i < array.length; i++){
-            meosse[i] = array[i];
-        }
+        for(int i =0; i < array.length; i++) meosse[i] = array[i];
         meosse[array.length] = height;
         
-        for(int i = array.length; i > 0; i--){                  // 내림차순 버블 정렬
-            for(int j = 0; j < i; j++){
+        for(int i = array.length; i > 0; i--)              // 내림차순 버블 정렬
+            for(int j = 0; j < i; j++)
                 if(meosse[j] < meosse[j+1]){
                     int temp = meosse[j];
                     meosse[j] = meosse[j+1];
                     meosse[j+1] = temp;
                 }
-            }
-        }
         for(int i = 0; i < meosse.length; i++){
             if(height != meosse[i]) answer++;
             else break;
         }
-        
         return answer;
     }
 }
